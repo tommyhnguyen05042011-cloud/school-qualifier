a359617bdd51053a575eefe67d1472c90e08b5de
TommyNguyen <tommyhnguyen05042011@gmail.com>
2026-09-17 16:23:58 +0700

    Move macro control to main.cpp for feature adding

--- include/func_add.hpp
+++ include/func_add.hpp
@@ -12,61 +12,12 @@ inline bool manual = false; /* state of control, manual or macroed.
                                when manual, driver will be able
                                to move the lift to whatever degree of movement he needs */
 
-/* macro commands to move the scoring subsystem to certain heights, a.k.a levels */
-inline void lift_lvsub0() {
-    lift.move(300 - lift.get_position() * -1);
-}
-inline void lift_lv0() {
-    lift.move(lift.get_position() * -1);
-}
-inline void lift_lv1() {
-    lift.move((1200 - lift.get_position()) * 1);
-}
-inline void lift_lv2() {
-    lift.move((2000 - lift.get_position()) * 1);
-}
-inline void lift_lv3() {
-    lift.move((2640 - lift.get_position()) * 1);
-}
-inline void lift_lv4() {
-    lift.move((3520 - lift.get_position()) * 1);
-}
-inline void lift_lv5() {
-    lift.move((4400 - lift.get_position()) * 1);
-}
-
 /* claw's position controlling macro. load is only available at lv 0, score is on all level */
 inline void claw_load() {
     bar.move((bar_rotation.get_position()) * -0.035);
 }
 inline void claw_score() {
-    bar.move((8600 - bar_rotation.get_position()) * 0.035);
-}
-
-// lift macro level control (run in a task)
-inline void macro_control() {
-    if (level == -1) {
-        lift_lvsub0();
-    } else if (level == 0) {
-        claw_load();
-        pros::delay(200);
-        lift_lv0();        
-    } else if (level == 1) {
-        lift_lv1();
-        claw_score();
-    } else if (level == 2) {
-        lift_lv2();
-        claw_score();
-    } else if (level == 3) {
-        lift_lv3();
-        claw_score();
-    } else if (level == 4) {
-        lift_lv4();
-        claw_score();
-    } else if (level == 5) {
-        lift_lv5();
-        claw_score();
-    }
+    bar.move((8600 - bar_rotation.get_position()) * 0.01);
 }
 
 /* scoring command */
--- src/main.cpp
+++ src/main.cpp
@@ -57,13 +57,45 @@ void autonomous() {}
 
 Controller master(E_CONTROLLER_MASTER);
 
-void opcontrol() {
-	bool clawState = false;
-	bool intake1State = false;
-	bool intake2State = false;
-	bool currentOpticalState = false;
-	bool lastOpticalState = false;
-	
+bool clawState = false;
+bool intake1State = false;
+bool intake2State = false;
+bool currentOpticalState = false;
+bool lastOpticalState = false;
+
+void macro_control() {
+    if (level == -1) {
+        lift.move(300 - lift.get_position() * 1);
+        claw_load();
+        pros::delay(100);
+        lift.move(300 - lift.get_position() * -1);
+    } else if (level == 0) {
+		if (clawState == false) {
+			claw_load();
+			delay(100);
+		} else {
+			claw_score();
+		}
+        lift.move(lift.get_position() * -1);
+    } else if (level == 1) {
+        lift.move((1200 - lift.get_position()) * 1);
+        claw_score();
+    } else if (level == 2) {
+        lift.move((2000 - lift.get_position()) * 1);;
+        claw_score();
+    } else if (level == 3) {
+        lift.move((2640 - lift.get_position()) * 1);
+        claw_score();
+    } else if (level == 4) {
+        lift.move((3520 - lift.get_position()) * 1);
+        claw_score();
+    } else if (level == 5) {
+        lift.move((4400 - lift.get_position()) * 1);
+        claw_score();
+    }
+}
+
+void opcontrol() {	
 	while (true) {
 		// get left y and right x positions
         int leftY = master.get_analog(E_CONTROLLER_ANALOG_LEFT_Y);
