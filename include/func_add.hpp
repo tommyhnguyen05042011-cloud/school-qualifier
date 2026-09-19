92c7f2b01aa9f48c2ef27149b7d72a4594a3160a
TommyNguyen <tommyhnguyen05042011@gmail.com>
2026-09-19 09:00:13 +0700

    P-gain tune

--- include/func_add.hpp
+++ include/func_add.hpp
@@ -22,7 +22,7 @@ inline bool lastOpticalDetect = false;
 
 /* claw's position controlling macro. load is only available at lv 0, score is on all level */
 inline void claw_load() {
-    bar.move(claw_rotation.get_position() * -0.03);
+    bar.move(claw_rotation.get_position() * -0.02);
 }
 inline void claw_score() {
     bar.move((8700 - claw_rotation.get_position()) * 0.03);
