9005ad9d0f305931dad1800d99f28ca56ec15268
TommyNguyen <tommyhnguyen05042011@gmail.com>
2026-09-23 16:49:17 +0700

    Adjust lateral PID controller's derivative gain from 10 to 5

--- include/subsystem.hpp
+++ include/subsystem.hpp
@@ -37,7 +37,7 @@ inline lemlib::OdomSensors sensors(&vertical_tracking_wheel, // vertical trackin
 // lateral PID controller
 inline lemlib::ControllerSettings lateral_controller(5, // proportional gain (kP)
                                                      0, // integral gain (kI)
-                                                     10, // derivative gain (kD)
+                                                     5, // derivative gain (kD)
                                                      0, // anti windup
                                                      0, // small error range, in inches
                                                      0, // small error range timeout, in milliseconds
