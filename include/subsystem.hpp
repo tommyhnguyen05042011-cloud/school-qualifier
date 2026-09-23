bb865bb582edd4c8d4d394e3f670afc971c1fba5
TommyNguyen <tommyhnguyen05042011@gmail.com>
2026-09-23 17:14:52 +0700

    Remove constants for PID tuning

--- include/subsystem.hpp
+++ include/subsystem.hpp
@@ -38,23 +38,23 @@ inline lemlib::OdomSensors sensors(&vertical_tracking_wheel, // vertical trackin
 inline lemlib::ControllerSettings lateral_controller(5, // proportional gain (kP)
                                                      0, // integral gain (kI)
                                                      3, // derivative gain (kD)
-                                                     3, // anti windup
-                                                     1, // small error range, in inches
-                                                     100, // small error range timeout, in milliseconds
-                                                     3, // large error range, in inches
-                                                     500, // large error range timeout, in milliseconds
-                                                     20 // maximum acceleration (slew)
+                                                     0, // anti windup
+                                                     0, // small error range, in inches
+                                                     0, // small error range timeout, in milliseconds
+                                                     0, // large error range, in inches
+                                                     0, // large error range timeout, in milliseconds
+                                                     0 // maximum acceleration (slew)
 );
 
 // angular PID controller
 inline lemlib::ControllerSettings angular_controller(3, // proportional gain (kP)
                                                      0, // integral gain (kI)
-                                                     11, // derivative gain (kD)
-                                                     0, // anti windup
-                                                     0, // small error range, in degrees
-                                                     0, // small error range timeout, in milliseconds
-                                                     0, // large error range, in degrees
-                                                     0, // large error range timeout, in milliseconds
+                                                     20, // derivative gain (kD)
+                                                     3, // anti windup
+                                                     1, // small error range, in degrees
+                                                     100, // small error range timeout, in milliseconds
+                                                     3, // large error range, in degrees
+                                                     500, // large error range timeout, in milliseconds
                                                      0 // maximum acceleration (slew)
 );
 
