779720b42748aec287d1ee632f1ec4ab7151f265
TommyNguyen <tommyhnguyen05042011@gmail.com>
2026-09-24 16:38:37 +0700

    Tune claw target

--- include/func_add.hpp
+++ include/func_add.hpp
@@ -56,19 +56,19 @@ inline void macro_lift() {
                     hold_score = false;
                 }
                 if (hold_score && clawClose) {
-                    clawTarget = 9200;
+                    clawTarget = 9000;
                 } else {
                     clawTarget = 0;
                 }
                 liftTarget = 0;
             } else if (level == 1) {
-                clawTarget = 9200;
+                clawTarget = 9000;
                 liftTarget = 1900;
             } else if (level == 2) {
-                clawTarget = 9200;
+                clawTarget = 9000;
                 liftTarget = 4500;
             } else if (level == 3) {
-                clawTarget = 9200;
+                clawTarget = 9000;
                 liftTarget  = 7100;
             }
             clawError = clawTarget - claw_rotation.get_position();
