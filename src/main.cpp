6da696fd5a4aafd5a9f66cb8faa27c7b0b737059
TommyNguyen <tommyhnguyen05042011@gmail.com>
2026-09-24 17:56:36 +0700

    Add actions

--- src/main.cpp
+++ src/main.cpp
@@ -110,6 +110,15 @@ void autonomous() {
 	clawTarget = 9000;
 	chassis.waitUntilDone();
 	claw_piston.set_value(true);
+
+	chassis.setPose(-40.17, -23.5, 90); // reset pose
+	delay(300);
+	clawTarget = 10500;
+	chassis.moveToPose(-37.1, -32.9, 188, 1300, {.minSpeed = 70, .earlyExitRange = 4});
+	delay(500);
+	clawTarget = 0;
+	liftTarget = 0;
+	chassis.moveToPose(-47, -47, 225, 1500);
 }
 
 Controller master(E_CONTROLLER_MASTER);
