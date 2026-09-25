4f648c4eb8c2fb7a89d7fcd117fb684567ff6e78
TommyNguyen <tommyhnguyen05042011@gmail.com>
2026-09-25 12:21:51 +0700

    Double intake motion to make sure stack goes into claw

--- src/main.cpp
+++ src/main.cpp
@@ -123,6 +123,10 @@ void autonomous() {
 	chassis.moveToPose(-47, -47, 225, 1500);
 	chassis.waitUntilDone();
 	intake_piston_back.set_value(true);
+	delay(300);
+	intake_piston_back.set_value(false);
+	delay(300);
+	intake_piston_back.set_value(true);
 	delay(1000);
 	claw_piston.set_value(false);
 	intake_piston_back.set_value(false);
